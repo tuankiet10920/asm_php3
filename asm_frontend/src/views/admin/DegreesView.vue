bf44db03a445f269b95f50164051f93462bb4e27
NguyenDinhIch <Nguyenich067@gmail.com>
2025-04-11 13:20:16 +0700

    Update DegreesView.vue

--- asm_frontend/src/views/admin/DegreesView.vue
+++ asm_frontend/src/views/admin/DegreesView.vue
@@ -142,7 +142,7 @@ onMounted(() => {
     <div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-          <div class="modal-header bg-primary text-white">
+          <div class="modal-header  text-white">
             <h5 class="modal-title">Sửa bằng cấp</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
           </div>
